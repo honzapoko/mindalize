0ad2e739ddc895e6a863279b354ca2185896f7a5
honzapoko <ing.honza.pokorny@gmail.com>
2025-07-05 20:21:45 +0200

    Update route.ts

--- src/app/api/create-checkout-session/route.ts
+++ src/app/api/create-checkout-session/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, { apiVersion: '2024-04-10' });
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 export async function POST(req: NextRequest) {
   const { email } = await req.json();
