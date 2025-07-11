920bf5e726ebe07962b69a7c8b92921fc5ec942f
honzapoko <ing.honza.pokorny@gmail.com>
2025-07-11 16:45:01 +0200

    Update TarotReading.tsx

--- src/components/TarotReading.tsx
+++ src/components/TarotReading.tsx
@@ -4,18 +4,18 @@ import Image from 'next/image';
 import cardMeanings from '../cardMeanings';
 
 const ZODIAC_SIGNS = [
-  { name: 'Aries', start: [3, 21], end: [4, 19] },
-  { name: 'Taurus', start: [4, 20], end: [5, 20] },
-  { name: 'Gemini', start: [5, 21], end: [6, 20] },
-  { name: 'Cancer', start: [6, 21], end: [7, 22] },
-  { name: 'Leo', start: [7, 23], end: [8, 22] },
-  { name: 'Virgo', start: [8, 23], end: [9, 22] },
-  { name: 'Libra', start: [9, 23], end: [10, 22] },
-  { name: 'Scorpio', start: [10, 23], end: [11, 21] },
-  { name: 'Sagittarius', start: [11, 22], end: [12, 21] },
-  { name: 'Capricorn', start: [12, 22], end: [1, 19] },
-  { name: 'Aquarius', start: [1, 20], end: [2, 18] },
-  { name: 'Pisces', start: [2, 19], end: [3, 20] },
+  { name: 'Beran', start: [3, 21], end: [4, 19] },
+  { name: 'Býk', start: [4, 20], end: [5, 20] },
+  { name: 'Blíženec', start: [5, 21], end: [6, 20] },
+  { name: 'Rak', start: [6, 21], end: [7, 22] },
+  { name: 'Lev', start: [7, 23], end: [8, 22] },
+  { name: 'Panna', start: [8, 23], end: [9, 22] },
+  { name: 'Váhy', start: [9, 23], end: [10, 22] },
+  { name: 'Škorpion', start: [10, 23], end: [11, 21] },
+  { name: 'Střelec', start: [11, 22], end: [12, 21] },
+  { name: 'Kozoroh', start: [12, 22], end: [1, 19] },
+  { name: 'Vodnář', start: [1, 20], end: [2, 18] },
+  { name: 'Ryby', start: [2, 19], end: [3, 20] },
 ];
 
 function getZodiacSign(dateStr: string) {
