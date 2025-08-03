ec0288166eeea43bdb292fb1ce6070334c227ea8
honzapoko <ing.honza.pokorny@gmail.com>
2025-08-03 15:03:08 +0200

    Update route.ts

--- src/app/api/send-daily-prophecy/route.ts
+++ src/app/api/send-daily-prophecy/route.ts
@@ -93,13 +93,13 @@ if (!isPremium && days >= 3) {
     </a>
   `;
 
-  if (!user || !user.email) {
+if (!user || !user.email) {
   return NextResponse.json({ error: 'Uživatel nebyl nalezen nebo nemá e-mail.' }, { status: 400 });
 }
 
   await resend.emails.send({
     from: 'vyklad@vesteni.cz',
-    to: user.email,
+    to: user!.email,
     subject: 'Získejte prémiové členství a pokračujte v denních proroctvích!',
     html: promoHtml,
   });
